be6207bd02dd586849a345d312c294f72950d98b
gxh <1527630987@qq.com>
2026-01-23 13:47:06 +0800

    refactor node

--- src/app/example/example.cpp
+++ src/app/example/example.cpp
@@ -32,16 +32,7 @@
 #include "packet/simple_data_packet.h"
 
 // Pipeline nodes 
-#include "nodes/Input/InputNode.h"
-#include "nodes/BMul/BMulNode.h"
-#include "nodes/CMul/CMulNode.h"
-#include "nodes/DAdd/DAddNode.h"
-#include "nodes/EMul/EMulNode.h"
-#include "nodes/FMul/FMulNode.h"
-#include "nodes/GSum/GSumNode.h"
-#include "nodes/HSum/HSumNode.h"
-#include "nodes/ISum/ISumNode.h"
-#include "nodes/Output/OutputNode.h"
+#include "nodes/custom_nodes.h"
 
 // Source and Consumer
 #include "source/simple_data_source.h"
---
+++ src/app/example/nodes/custom_nodes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Pipeline nodes (example)
+#include "nodes/Input/InputNode.h"
+#include "nodes/BMul/BMulNode.h"
+#include "nodes/CMul/CMulNode.h"
+#include "nodes/DAdd/DAddNode.h"
+#include "nodes/EMul/EMulNode.h"
+#include "nodes/FMul/FMulNode.h"
+#include "nodes/GSum/GSumNode.h"
+#include "nodes/HSum/HSumNode.h"
+#include "nodes/ISum/ISumNode.h"
+#include "nodes/Output/OutputNode.h"
+
