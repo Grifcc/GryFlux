19ee8e1af4db5ac3c93b1139e106be3ab8223716
zjx <jxzhao1106@gmail.com>
2026-04-15 16:56:03 +0800

    Remove resnet_Atlas from ZeroDCE_Atlas branch

--- src/app/resnet_Atlas/nodes/Preprocess/PreprocessNode.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "framework/async_pipeline.h"
-#include "framework/node_base.h"
-
-class PreprocessNode : public GryFlux::NodeBase {
-public:
-    void execute(GryFlux::DataPacket &packet, GryFlux::Context &ctx) override;
-};
